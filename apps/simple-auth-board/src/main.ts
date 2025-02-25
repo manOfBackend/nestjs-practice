3cab5d52ddb49d92c475aa6df98df4ce8c07e29d
manOfBackend <man.of.backend@gmail.com>
2025-02-25 23:13:34 +0900

    feat: add response interceptor for standardized API responses

--- apps/simple-auth-board/src/main.ts
+++ apps/simple-auth-board/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { SimpleAuthBoardModule } from './simple-auth-board.module';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -19,6 +20,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(3000);
   console.log(`Application is running on: http://localhost:3000`);
